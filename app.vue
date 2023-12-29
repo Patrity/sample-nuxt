399120f128b669ad457b4d3922b9f3a9edc21129
Tony Costanzo <tonycos92@gmail.com>
2023-12-29 17:07:57 -0600

    Initial Commit

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+<script setup lang="ts">
+const client = useSupabaseClient()
+
+import {definePageMeta} from "nuxt/dist/pages/runtime";
+
+definePageMeta({
+  middleware: 'auth',
+})
+
+const count = ref(0)
+onMounted(() => {
+	count.value = 1
+})
+const increment = () => {
+	count.value++
+}
+const decrement = () => {
+	count.value--
+}
+</script>
---
+++ components/SampleComponent.vue
@@ -0,0 +1,14 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+	msg: {
+		type: String,
+		required: true
+	}
+})
+const emit = defineEmits(['update:msg'])
+const msg = useVModel(props, 'msg', emit)
+</script>
